af45667b5fcae74b322db8c89d17144d20496974
nvnreddynaini <nvnreddynaini@gmail.com>
2022-12-30 13:06:13 +0530

    fourth commit

--- src/main/java/com/driver/StudentService.java
+++ src/main/java/com/driver/StudentService.java
@@ -11,39 +11,39 @@ public class StudentService {
     @Autowired
     StudentRepository studentRepository;
 
-    public void addAStudent(Student student){
+    public void addStudent(Student student){
         studentRepository.addStudentFromDb(student);
     }
 
-    public void addATeacher(Teacher teacher){
+    public void addTeacher(Teacher teacher){
         studentRepository.addTeacherFromDb(teacher);
     }
 
-    public void addAStudentTeacherPair(String student, String teacher){
+    public void addStudentTeacherPair(String student, String teacher){
         studentRepository.addStudentTeacherPairFromDb(student,teacher);
     }
 
-    public Student getAStudentByName(String name){
+    public Student getStudentByName(String name){
         return studentRepository.getStudentByNameFromDb(name);
     }
 
-    public Teacher getATeacherByName(String name){
+    public Teacher getTeacherByName(String name){
         return studentRepository.getTeacherByNameFromDb(name);
     }
 
-    public List<String> getAStudentsByTeacherName(String teacher){
+    public List<String> getStudentsByTeacherName(String teacher){
         return studentRepository.getStudentsByTeacherNameFromDb(teacher);
     }
 
-    public List<String> getAAllStudents(){
+    public List<String> getAllStudents(){
         return studentRepository.getAllStudentsFromDb();
     }
 
-    public void deleteATeacherByName(String teacher){
+    public void deleteTeacherByName(String teacher){
         studentRepository.deleteTeacherByNameFromDb(teacher);
     }
 
-    public void deleteAAllTeachers(){
+    public void deleteAllTeachers(){
         studentRepository.deleteAllTeachersFromDb();
     }
 
